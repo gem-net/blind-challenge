a1fb0580d212a89d6c5fe4cf0900110d3acc3a74
Stephen Gaffney <stephen.gaffney@yale.edu>
2019-01-14 11:44:09 +0000

    Initial commit

---
+++ tests/test_skeleton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from reviewers.skeleton import fib
+
+__author__ = "Stephen Gaffney"
+__copyright__ = "Stephen Gaffney"
+__license__ = "gpl3"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
