c2eb904db4f70a1cdb07122b9ef4316c87a4b892
Stephen Gaffney <stephen.gaffney@yale.edu>
2019-10-18 14:33:20 +0100

    Use PasswordField for password input.

--- src/blind_challenge/app/forms.py
+++ src/blind_challenge/app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Submit')
